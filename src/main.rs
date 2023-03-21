adebc163125993b511144e882d4e7aaef0e25bb6
Tyler Wojciechowski <tylerwojo81@gmail.com>
2023-03-21 15:42:45 -0500

    switch use of 'lexeme' terminology to 'token'

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,42 @@
 #![allow(non_camel_case_types)]
 
 #[derive(Clone)]
-enum LexemeKind {
+enum TokenKind {
     identifier, number, operator, punctuation
 }
 
 #[derive(Clone)]
-struct Lexeme {
+struct Token {
     content: String,
-    kind: LexemeKind,
+    kind: TokenKind,
 }
 
-impl Lexeme {
-    fn new(content: String, kind: LexemeKind) -> Self {
+impl Token {
+    fn new(content: String, kind: TokenKind) -> Self {
         Self{content, kind}
     }
 }
 
 trait Source {
-    fn get_current(&self) -> Lexeme;
+    fn get_current(&self) -> Token;
     fn advance(&mut self);
     fn is_valid(&self) -> bool;
 }
 
 struct SimpleSource {
-    lexemes: Vec<Lexeme>,
+    tokens: Vec<Token>,
     index: usize,
 }
 
 impl SimpleSource {
-    fn new(lexemes: Vec<Lexeme>) -> Self {
-        Self{lexemes, index: 0}
+    fn new(tokens: Vec<Token>) -> Self {
+        Self{tokens, index: 0}
     }
 }
 
 impl Source for SimpleSource {
-    fn get_current(&self) -> Lexeme {
-        self.lexemes[self.index].clone()
+    fn get_current(&self) -> Token {
+        self.tokens[self.index].clone()
     }
 
     fn advance(&mut self) {
@@ -44,13 +44,13 @@ impl Source for SimpleSource {
     }
 
     fn is_valid(&self) -> bool {
-        self.index != self.lexemes.len()
+        self.index != self.tokens.len()
     }
 }
 
 struct StringSource {
     string: String,
-    lexeme: Option<Lexeme>,
+    token: Option<Token>,
     index: usize,
 }
 
@@ -76,7 +76,7 @@ impl StringSource {
     fn new(string: String) -> Self {
         let mut source = Self {
             string,
-            lexeme: None,
+            token: None,
             index: 0,
         };
         source.advance();
@@ -104,24 +104,24 @@ impl StringSource {
         self.index += count;
     }
 
-    fn get_number(&self) -> Lexeme {
+    fn get_number(&self) -> Token {
         let count = self.count(is_digit_or_dot);
-        Lexeme::new(self.string[self.index..(self.index + count)].into(), LexemeKind::number)
+        Token::new(self.string[self.index..(self.index + count)].into(), TokenKind::number)
     }
 
-    fn get_single(&self, kind: LexemeKind) -> Lexeme {
-        Lexeme::new(self.string[self.index..(self.index + 1)].into(), kind)
+    fn get_single(&self, kind: TokenKind) -> Token {
+        Token::new(self.string[self.index..(self.index + 1)].into(), kind)
     }
 
-    fn get_lexeme(&mut self) -> Option<Lexeme> {
+    fn get_token(&mut self) -> Option<Token> {
         if self.view().is_empty() {
             None
         } else if self.view().starts_with(char::is_numeric) {
             Some(self.get_number())
         } else if self.view().starts_with(is_operator) {
-            Some(self.get_single(LexemeKind::operator))
+            Some(self.get_single(TokenKind::operator))
         } else if self.view().starts_with(is_punctuation) {
-            Some(self.get_single(LexemeKind::punctuation))
+            Some(self.get_single(TokenKind::punctuation))
         } else {
             panic!("unexpected character encountered")
         }
@@ -129,27 +129,27 @@ impl StringSource {
 }
 
 impl Source for StringSource {
-    fn get_current(&self) -> Lexeme {
-        self.lexeme.clone().unwrap()
+    fn get_current(&self) -> Token {
+        self.token.clone().unwrap()
     }
 
     fn advance(&mut self) {
         self.skip_whitespace();
-        let lexeme = self.get_lexeme();
-        if let Some(lexeme) = &lexeme {
-            self.index += lexeme.content.len();
+        let token = self.get_token();
+        if let Some(token) = &token {
+            self.index += token.content.len();
         }
-        self.lexeme = lexeme;
+        self.token = token;
     }
 
     fn is_valid(&self) -> bool {
-        self.lexeme.is_some()
+        self.token.is_some()
     }
 }
 
 struct Yard {
-    expression: Vec<Lexeme>,
-    stack: Vec<Lexeme>,
+    expression: Vec<Token>,
+    stack: Vec<Token>,
 }
 
 impl Yard {
@@ -157,11 +157,11 @@ impl Yard {
         Self{expression: Vec::new(), stack: Vec::new()}
     }
 
-    fn add_number(&mut self, number: Lexeme) {
+    fn add_number(&mut self, number: Token) {
         self.expression.push(number);
     }
 
-    fn pop_higher_operator(&mut self, precedence: usize) -> Option<Lexeme> {
+    fn pop_higher_operator(&mut self, precedence: usize) -> Option<Token> {
         if self.stack.last().filter(|stack_operator| precedence_of(&stack_operator.content) >= precedence).is_some() {
             self.stack.pop()
         } else {
@@ -169,11 +169,11 @@ impl Yard {
         }
     }
 
-    fn pop_operator(&mut self) -> Option<Lexeme> {
+    fn pop_operator(&mut self) -> Option<Token> {
         self.stack.pop()
     }
 
-    fn add_operator(&mut self, operator: Lexeme) {
+    fn add_operator(&mut self, operator: Token) {
         let precedence = precedence_of(&operator.content);
         while let Some(stack_operator) = self.pop_higher_operator(precedence) {
             self.expression.push(stack_operator);
@@ -181,7 +181,7 @@ impl Yard {
         self.stack.push(operator);
     }
 
-    fn add_left_paren(&mut self, paren: Lexeme) {
+    fn add_left_paren(&mut self, paren: Token) {
         self.stack.push(paren);
     }
 
@@ -214,16 +214,16 @@ fn precedence_of(operator: &str) -> usize {
     }
 }
 
-fn handle_edge(yard: &mut Yard, lexeme: &Lexeme) -> bool {
-    use LexemeKind::*;
-    match lexeme.kind {
+fn handle_edge(yard: &mut Yard, token: &Token) -> bool {
+    use TokenKind::*;
+    match token.kind {
         number => {
-            yard.add_number(lexeme.clone());
+            yard.add_number(token.clone());
             true
         },
         punctuation => {
-            match lexeme.content.as_str() {
-                "(" => yard.add_left_paren(lexeme.clone()),
+            match token.content.as_str() {
+                "(" => yard.add_left_paren(token.clone()),
                 ")" => panic!("did not expect ')'"),
                 _ => panic!("unexpected token")
             }
@@ -233,15 +233,15 @@ fn handle_edge(yard: &mut Yard, lexeme: &Lexeme) -> bool {
     }
 }
 
-fn handle_middle(yard: &mut Yard, lexeme: &Lexeme) -> bool {
-    use LexemeKind::*;
-    match lexeme.kind {
+fn handle_middle(yard: &mut Yard, token: &Token) -> bool {
+    use TokenKind::*;
+    match token.kind {
         operator => {
-            yard.add_operator(lexeme.clone());
+            yard.add_operator(token.clone());
             true
         },
         punctuation => {
-            match lexeme.content.as_str() {
+            match token.content.as_str() {
                 ")" => yard.add_right_paren(),
                 "(" => panic!("did not expect '('"),
                 _ => panic!("unexpected token")
@@ -254,8 +254,8 @@ fn handle_middle(yard: &mut Yard, lexeme: &Lexeme) -> bool {
 
 type BinaryOperation = fn(f32, f32) -> f32;
 
-fn function_of(lexeme: &Lexeme) -> BinaryOperation {
-    match lexeme.content.as_str() {
+fn function_of(token: &Token) -> BinaryOperation {
+    match token.content.as_str() {
         "+" => |a, b| a + b,
         "-" => |a, b| a - b,
         "*" => |a, b| a * b,
@@ -266,15 +266,15 @@ fn function_of(lexeme: &Lexeme) -> BinaryOperation {
 
 fn evaluate(yard: &Yard) -> f32 {
     let mut slots = Vec::<f32>::new();
-    for lexeme in &yard.expression {
-        use LexemeKind::*;
-        match lexeme.kind {
-            number => slots.push(lexeme.content.parse().unwrap()),
+    for token in &yard.expression {
+        use TokenKind::*;
+        match token.kind {
+            number => slots.push(token.content.parse().unwrap()),
             operator => {
                 let right = slots.pop().unwrap();
                 let left = *slots.last().unwrap();
 
-                *slots.last_mut().unwrap() = function_of(lexeme)(left, right);
+                *slots.last_mut().unwrap() = function_of(token)(left, right);
             },
             _ => panic!("wrong token"),
         }
@@ -295,13 +295,13 @@ fn main() {
         let mut yard = Yard::new();
 
         while source.is_valid() {
-            let lexeme = source.get_current();
+            let token = source.get_current();
             if is_edge {
-                if handle_edge(&mut yard, &lexeme) {
+                if handle_edge(&mut yard, &token) {
                     is_edge = false;
                 }
             } else {
-                if handle_middle(&mut yard, &lexeme) {
+                if handle_middle(&mut yard, &token) {
                     is_edge = true;
                 }
             }
